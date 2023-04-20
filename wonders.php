5c6dbdbcccfd7ce15cd2e2be30c4d9d1f5633a4c
Amos Johnson <63177847+AmosJAMcLovin@users.noreply.github.com>
2023-04-20 14:29:02 -0400

    Add files via upload

---
+++ wonders.php
@@ -0,0 +1,32 @@
+<DOCTYPE! html>
+<html>
+<head>
+</head>
+
+<body>
+<?php
+echo "<h2>Seven Natural Wonders of Canada</h2>"; 
+// Create bulletin list of standout locations in Canada
+echo " <li>Niagara Falls, Ontario</li>";
+echo " <li>Bay of Fundy, the Maritimes</li>";
+echo " <li>Rocky Mountains, British Columbia / Alberta</li>";
+echo " <li>Nahanni National Park Reserve, Northwest Territories</li>";
+echo " <li>Gros Morne National Park, Newfoundland</li>";
+echo " <li>Dinosaur Provincial Park, Alberta</li>";
+echo " <li>Northern Lights</li>"; 
+
+echo "<br></br>"; 
+
+// Display images of some of Canada's standout locations
+echo "<i>Images:</i> <br> ";
+echo '<img src="wondersImages/niagarafalls.jpg" alt="Niagara Falls"/>';
+echo '<img src="wondersImages/rockies.jpg" alt="Rockies"/>'; 
+echo '<img src="wondersImages/northernlights.jpg" alt="Northern Lights"/>';
+
+echo "<br></br>";
+// Byline
+echo '<p>Programmed by - "Amos Johnson"</p> ';
+?>
+
+</body>
+</html>
